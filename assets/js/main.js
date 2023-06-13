8b1666130ea42c1f8623397e23db25ffbdaad236
MateusPavesi <mateus.pavesi@hotmail.com>
2023-06-13 17:01:37 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,75 @@
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerHTML = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBtnApagar(li);
+    salvarTarefas();
+}
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBtnApagar(li) {
+    li.innerHTML += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerHTML = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    li.appendChild(botaoApagar);
+}
+
+btnTarefa.addEventListener('click', function () {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if (el.classList.contains('apagar')) {
+        el.parentElement.remove();
+        salvarTarefas();
+    }
+});
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaTarefas = [];
+
+    for (let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaTarefas.push(tarefaTexto);
+    }
+    
+    const tarefasJSON = JSON.stringify(listaTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();
